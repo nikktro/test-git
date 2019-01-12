879a11669503d93edf6084d527af32b827499920
Nikolay Trofimov <nikktro@gmail.com>
2019-01-12 21:19:18 +0300

    Initial Commit

---
+++ test-git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-git
+//
+//  Created by Nikolay.Trofimov on 12/01/2019.
+//  Copyright © 2019 Nikolay.Trofimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
